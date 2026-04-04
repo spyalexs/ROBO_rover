29b73a7a3a5d5a2e8ba5623269233466acc59b08
kranzowe <kranzowe@gmail.com>
2026-04-04 12:38:34 -0600

    removing stuff

--- robo_rover/launch/rover_launch.py
+++ robo_rover/launch/rover_launch.py
@@ -4,30 +4,14 @@ ROS2 Launch file for ROBO Rover
 Launches the rover node with configurable parameters
 """
 
-import os
 from launch.actions import DeclareLaunchArgument, LogInfo
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    package_share_dir = get_package_share_directory('robo_rover')
-    urdf_file = os.path.join(package_share_dir, 'urdf', 'simple.urdf')
 
-    with open(urdf_file, 'r') as f:
-        robot_description_content = f.read()
-
-    robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='robot_state_publisher',
-        output='screen',
-        parameters=[{
-            'robot_description': robot_description_content
-        }],
-    )
 
     # Declare launch arguments
     connection_string_arg = DeclareLaunchArgument(
@@ -75,22 +59,7 @@ def generate_launch_description():
             'imu_frequency': LaunchConfiguration('imu_frequency'),
         }],
     )
-    # Static TF node
-    static_tf_node = Node(
-    package='tf2_ros',
-    executable='static_transform_publisher',
-    name='base_link_to_laser',
-    arguments=[
-        '--x', '-0.0251', 
-        '--y', '0.0', 
-        '--z', '0.1683', 
-        '--yaw', '0', 
-        '--pitch', '0', 
-        '--roll', '0', 
-        '--frame-id', 'base_link', 
-        '--child-frame-id', 'laser'
-    ]
-    )
+
     # Log info about the launch
     log_info = LogInfo(
         msg=[
@@ -110,6 +79,5 @@ def generate_launch_description():
         namespace_arg,
         log_info,
         rover_node,
-        static_tf_node,
-        robot_state_publisher_node,
+
     ])
